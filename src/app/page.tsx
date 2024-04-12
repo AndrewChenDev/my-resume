18fbaa03997e321cbac3e946f68e4b2415cea308
andrew840513 <andrew@ionblaster.com>
2024-04-12 03:06:20 -0700

    change margin to have more space between

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ import {
 export default function Home() {
   return (
     <>
-      <BasicInfo />
+      <BasicInfo className={"mb-4"} />
       <hr />
       <Contacts />
       <hr />
 
-      <div className={"flex flex-row gap-[3.5rem]"}>
+      <div className={"flex flex-row gap-[3.5rem] mt-4"}>
         <div className="left w-1/2 flex flex-col gap-2.5">
           <Experiences />
           <Educations />
--- src/sections/top/BasicInfo.tsx
+++ src/sections/top/BasicInfo.tsx
@@ -3,7 +3,7 @@ import { Name, SummaryStatement, Title } from "@/components";
 export default function BasicInfo({ className = "" }: { className?: string }) {
   return (
     <section className={`top-section ${className}`}>
-      <Name>Che An (Andrew) Chen</Name>
+      <Name className={"mb-2"}>Che An (Andrew) Chen</Name>
       <Title>Full Stack Developer</Title>
       <SummaryStatement>
         An experienced web and software developer with ability work under
